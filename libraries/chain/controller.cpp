d18560c8971180048ed2748df1acbe29ed11f0a8
huachen <huachen@gmail.com>
2020-03-02 09:09:18 +0800

    do not check tapos

--- libraries/chain/controller.cpp
+++ libraries/chain/controller.cpp
@@ -3135,12 +3135,18 @@ void controller::validate_expiration( const transaction& trx )const { try {
 } FC_CAPTURE_AND_RETHROW((trx)) }
 
 void controller::validate_tapos( const transaction& trx )const { try {
+
+   return;
+
+   // do not check ref block for eos v2, HC
+   /*
    const auto& tapos_block_summary = db().get<block_summary_object>((uint16_t)trx.ref_block_num);
 
    //Verify TaPoS block summary has correct ID prefix, and that this block's time is not past the expiration
    EOS_ASSERT(trx.verify_reference_block(tapos_block_summary.block_id), invalid_ref_block_exception,
               "Transaction's reference block did not match. Is this transaction from a different fork?",
               ("tapos_summary", tapos_block_summary));
+   */
 } FC_CAPTURE_AND_RETHROW() }
 
 void controller::validate_db_available_size() const {
